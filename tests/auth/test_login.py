ee7073bcaf9a565254be55ed152c04ee7ba2063b
Michael Shkryabin <mihail.shkryabin@gmail.com>
2025-02-27 12:25:42 +0300

    Добавлена проверка обработки ошибки при вводе некорректной пары логин/пароль

--- tests/auth/test_login.py
+++ tests/auth/test_login.py
@@ -29,3 +29,21 @@ def test_login(page):  # test_ в начале обязательно!
     logout_button.wait_for(state="visible")
     expect(logout_button, "Кнопка 'Выйти' не отобраажется на странице").to_be_visible()
     logout_button.click()
+
+def test_wrong_login(page):
+    page.goto("https://yobamos.veilstaff.com/login?returnUrl=%2Ffeed")
+
+    login_input = page.get_by_placeholder("Логин")
+    login_input.click()
+    login_input.fill("wronglogin")
+
+    password_input = page.get_by_placeholder("Пароль")
+    password_input.click()
+    password_input.fill("wrongpassword")
+
+    login_button = page.get_by_role("button", name="Войти")
+    login_button.click()
+
+    expect(login_input, "Поле 'Логин' не подсвечивается розовым").to_have_attribute("aria-invalid", "true")
+    expect(password_input, "Поле 'Пароль' не подсвечивается розовым").to_have_attribute("aria-invalid", "true")
+    expect(page.get_by_text("Логин или пароль введены неверно")).to_be_visible()
